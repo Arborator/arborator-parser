06f5c3d92c31d4a5d221159ed63facdbbb51390f
kirianguiller <kirianguiller@gmail.com>
2023-06-30 11:43:42 +0200

    scripts: update train_parser_template script

--- train_parser_template.sh
+++ train_parser_template.sh
@@ -1,8 +1,9 @@
 /home/arboratorgrew/miniconda3/envs/kirian/bin/python3 \
     /home/arboratorgrew/arborator-parser/BertForDeprel/BertForDeprel/run.py train \
     --ftrain /home/arboratorgrew/SUD_all_treebanks_utilities/data/all_together/ \
-    --model_folder_path /home/arboratorgrew/arborator-parser_models_prod/_PRETRAINED/SUD_all_native_treebaks \
+    --model_folder_path /home/arboratorgrew/arborator-parser_models_prod/_PRETRAINED/SUD_all_native_treebaks_GLOT_500_lr00005 \
     --batch_size 16  \
     --max_epoch 150  \
     --patience 10  \
-    --split_ratio 0.98
+    --split_ratio 0.98 \
+    --embedding_type cis-lmu/glot500-base
